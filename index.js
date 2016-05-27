48197dc27471f18fc2ad95d5f3075d6582492ab4
Sushant <sushantdhiman@outlook.com>
2016-05-27 17:52:03 +0530

    fixes update and SEQ_LOG

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-process.env.DEBUG = process.env.DEBUG || 'sequelize:perf';
+process.env.DEBUG = process.env.DEBUG || 'sequelize:perf*';
 
 /** Sequelize Benchmark **/
 require('./lib').run();
